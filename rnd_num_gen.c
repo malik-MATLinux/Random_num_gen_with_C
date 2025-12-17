ab38b53210a737f3b9163b38689e3aeffd6403b9
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-17 22:51:44 +0000

    Generate between 50 - 100

--- rnd_num_gen.c
+++ rnd_num_gen.c
@@ -11,10 +11,10 @@ int main(){
     srand(time(NULL)); // set the base seed value based on the time
 
     //* trying to get random number between 1 and 6
-    int min = 1;
-    int max = 6;
+    int min = 50;
+    int max = 100;
 
-    int randomNum =  (rand() % max) + min;
+    int randomNum =  (rand() % (max - min + 1)) + min;
 
     printf("%d \n\n", randomNum);
     return 0;
