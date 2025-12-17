79373c1dd9a9ca6f183f1e6d41074e948d9b61b0
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-17 22:31:31 +0000

    Getting random number between 0 - 2 by getting the result of current seed value modulus 2, increment by 1

--- rnd_num_gen.c
+++ rnd_num_gen.c
@@ -5,5 +5,15 @@
 
 
 int main(){
+
+    //* First of all, call the random function.
+
+    srand(time(NULL)); // set the base seed value based on the time
+
+    int min = 1;
+    int max = 6;
+    int randomNum =  (rand() % max) + min;
+
+    printf("%d \n\n", randomNum);
     return 0;
 }
