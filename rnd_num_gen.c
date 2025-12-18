a823240f835ee13be79a965bf65c253e186907a8
Malik Adebayo <malik.adebayo@matlinux.com>
2025-12-18 08:28:38 +0000

    Dice simulation

--- rnd_num_gen.c
+++ rnd_num_gen.c
@@ -10,10 +10,11 @@ int main(){
 
     srand(time(NULL)); // set the base seed value based on the time
 
-    //* trying to get random number between 1 and 6
+    //* trying to get random number between 50 and 100
     int min = 50;
     int max = 100;
 
+    //* Making it 3 random guesses to simulate a dice.
     int randomNum1 =  (rand() % (max - min + 1)) + min;
     int randomNum2 =  (rand() % (max - min + 1)) + min;
     int randomNum3 =  (rand() % (max - min + 1)) + min;
